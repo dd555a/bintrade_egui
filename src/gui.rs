c2a862a6c565af950b96205a5566b5c9e4ab0f57
DB <dd555@protonmail.com>
2026-02-02 15:08:48 +0000

    Update DATA manager GUI window

--- src/gui.rs
+++ src/gui.rs
@@ -2076,7 +2076,10 @@ struct DataManager{
 
     max_backdate_months:usize,
 
-    autoupdate_on_start:bool
+    autoupdate_on_start:bool,
+    update_success:bool,
+    update_ran:bool,
+    update_status:String,
 
 }
 
@@ -2085,6 +2088,8 @@ impl DataManager{
         DataManager{
             shortlist_max:10,
             max_backdate_months:120,
+            update_status:"Not ran".to_string(),
+            update_ran:false,
             ..Default::default()
         }
     }
@@ -2128,13 +2133,79 @@ impl DataManager{
                 //NOTE Send client instruction
             }
         });
+        ui.style_mut().visuals.selection.bg_fill = Color32::GREEN;
+        ui.style_mut().visuals.widgets.inactive.bg_fill = Color32::RED;
+        ui.style_mut().visuals.widgets.hovered.bg_fill = Color32::RED;
+        ui.style_mut().visuals.widgets.active.bg_fill = Color32::RED;
+
+
         ui.end_row();
+        match data_manager.update_success{
+            true => {
+                ui.label(
+                    RichText::new(format!["Autoupdate successful: {}", data_manager.update_status])
+                        .color(Color32::GREEN),
+                );
+            }
+            false =>{
+                match data_manager.update_ran{
+                    true => {
+                        ui.label(
+                            RichText::new(format!["Autoupdate failed: {}", data_manager.update_status])
+                                .color(Color32::RED),
+                        );
+                    }
+                    false => {
+                        ui.label(
+                            RichText::new(format!["Update not ran"])
+                                .color(Color32::ORANGE),
+                        );
+                    }
+                }
+            }
+        };
         if ui.button("Update all data").clicked() {
             //NOTE Send client instruction
+            //TODO add a progress bar and result
+            //
 
         }
+
+        toggle_ui_compact(ui,&mut data_manager.update_success);
         ui.end_row();
     }
+}
+
+fn toggle_ui_compact(ui: &mut egui::Ui, on: &mut bool) -> egui::Response {
+    let desired_size = ui.spacing().interact_size.y * egui::vec2(2.0, 1.0);
+    let (rect, mut response) = ui.allocate_exact_size(desired_size, egui::Sense::click());
+    if response.clicked() {
+        *on = !*on;
+        response.mark_changed();
+    }
+    response.widget_info(|| {
+        egui::WidgetInfo::selected(egui::WidgetType::Checkbox, ui.is_enabled(), *on, "")
+    });
+
+    if ui.is_rect_visible(rect) {
+        let how_on = ui.ctx().animate_bool_responsive(response.id, *on);
+        let visuals = ui.style().interact_selectable(&response, *on);
+        let rect = rect.expand(visuals.expansion);
+        let radius = 0.5 * rect.height();
+        ui.painter().rect(
+            rect,
+            radius,
+            visuals.bg_fill,
+            visuals.bg_stroke,
+            egui::StrokeKind::Inside,
+        );
+        let circle_x = egui::lerp((rect.left() + radius)..=(rect.right() - radius), how_on);
+        let center = egui::pos2(circle_x, rect.center().y);
+        ui.painter()
+            .circle(center, 0.75 * radius, visuals.bg_fill, visuals.bg_stroke);
+    }
+
+    response
 }
 
 
